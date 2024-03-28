4f9c2cc6b0d149f8f833d92a849e2b80ec327755
Lucas Henrique <120137183+LHenrique2502@users.noreply.github.com>
2024-03-28 15:11:35 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Tabuada();
+
+static void Tabuada()
+{
+    Console.WriteLine("Escolha o número da tabuada:");
+    float n1 = float.Parse(Console.ReadLine());
+
+    for (int contador = 0; contador <= 10; contador++)
+    {
+        Console.WriteLine($"{n1} X {contador} = {n1 * contador}");
+    }
+
+}
+Console.WriteLine("Fim da tabuada!");
+Console.WriteLine("Para gerar outra tabuada pressione 1, para sair digite 0");
+Console.ReadKey();
+
+short res = short.Parse(Console.ReadLine());
+switch (res)
+{
+    case 1: Tabuada(); break;
+    case 0: System.Environment.Exit(0); break;
+    default: Tabuada(); break;
+
+}
